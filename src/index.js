db0bbdac6f3bd46a525ca6d36f941afb69b1a4f2
Cristian Rojas <work@cristianrojas.com>
2019-03-02 12:06:04 -0500

    React basic configuration.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Cool meme');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>{title}</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
